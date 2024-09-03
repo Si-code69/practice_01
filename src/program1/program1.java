cd42ad0cac912a606cb218b0f49698c56b19e176
Si <sygmememe@gmail.com>
2024-09-03 14:17:03 +0800

    initial commit

---
+++ src/program1/program1.java
@@ -0,0 +1,20 @@
+package program1;
+
+import java.util.Scanner;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+		//Scanner in = new Scanner(System.in);
+		//String X = in.nextLine();
+		//System.out.println(X + " my ass");
+		
+		char c = '草';
+		System.out.println(c);
+		
+	}
+	
+	String x = new String(""); 
+}
